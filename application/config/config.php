b75aae6e5304cd3ed1b3afe29024da0484668929
root <root@test.virtualizor.com>
2018-11-09 15:57:22 +0700

    remove warning while access from cli

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root = "http://".$_SERVER['HTTP_HOST'];
+if (isset($_SERVER['HTTP_HOST'])) {
+  $root = "http://".$_SERVER['HTTP_HOST'];
+}else{
+	$root = '';
+}
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 $config['base_url']    = "$root";
 
